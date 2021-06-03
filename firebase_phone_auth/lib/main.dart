06b1b702609c4651eb5e7d98c811ede2f38f45f2
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-06-03 22:35:42 +0500

    Update main.dart

--- firebase_phone_auth/lib/main.dart
+++ firebase_phone_auth/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(pushed 123);
+  WidgetsFlutterBinding.ensureInitialized(pushed 666 123);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
