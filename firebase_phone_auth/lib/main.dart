dd01f89f5f5472cf2c31fbdf94d44b25a42e5f43
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-09-20 16:00:55 +0500

    Update main.dart

--- firebase_phone_auth/lib/main.dart
+++ firebase_phone_auth/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: loginScreen(),
-      theme: ThemeData(  asasas
+      theme: ThemeData(  
       ),
     );
   }
